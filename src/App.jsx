9501dc6439581bb9587e344f9e3b9200a64b159b
Sivareddy2147 <abimanyudu147@gmail.com>
2025-09-08 15:20:49 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Movies from "./movies.jsx";
+function App() {
+  return (
+    <div class="border border-2 border-danger p-2 m-3">
+      <h2>Hello Edupoly</h2>
+      <Movies></Movies>
+    </div>
+  );
+}
+
+export default App;
