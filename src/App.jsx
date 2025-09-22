ee8f990d7e60a2206720446e1c214aba1470308e
Sivareddy2147 <abimanyudu147@gmail.com>
2025-09-22 15:56:28 +0530

    ToggleLight

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import Movies from "./movies.jsx";
+import ToggleLight from "./ToggleLight.jsx";
 function App() {
   return (
     <div class="border border-2 border-danger p-2 m-3">
-      <h2>Hello Edupoly</h2>
-      
-    
+      <h2>Reactjs - Practice </h2>
+      <ToggleLight></ToggleLight>
     </div>
   );
 }
